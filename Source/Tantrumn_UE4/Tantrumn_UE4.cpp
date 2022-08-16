d22524dfe70898e0a68bd6735a7ad9275ceac610
hoantran-ABK <109544327+hoantran-ABK@users.noreply.github.com>
2022-08-16 10:56:27 -0700

    created placeholder MyActor to generate VS solution

---
+++ Source/Tantrumn_UE4/Tantrumn_UE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tantrumn_UE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tantrumn_UE4, "Tantrumn_UE4" );
